17f37f3e7654973ed2523c2fc22df74cc735ed55
bluehoneygee <bluehoneygee@gmail.com>
2025-11-10 21:40:49 +0700

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <h1 className="font-bold font-inter text-5xl">Welcome</h1>
+      <h1 className=" font-grotesk text-5xl">Welcome</h1>
+      <h1 className=" text-5xl">Welcome</h1>
+      <h1 className=" text-5xl font-inter">Welcome</h1>
+    </>
+  );
 };
 
 export default HomePage;
