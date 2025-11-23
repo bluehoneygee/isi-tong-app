8196ff7b46f25360e468b133e5acd1f95c7c8f15
bluehoneygee <bluehoneygee@gmail.com>
2025-11-23 10:16:09 +0700

    Implement Stats UI

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { RouteParams } from "@/types/global";
 import UserAvatar from "@/components/ui/UserAvatar";
+import Stats from "@/components/user/Stats";
 
 const Profile = async ({ params }: RouteParams) => {
   const { id } = await params;
@@ -31,57 +32,64 @@ const Profile = async ({ params }: RouteParams) => {
     user;
 
   return (
-    <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
-      <div className="flex flex-col items-start gap-4 lg:flex-row">
-        <UserAvatar
-          id={_id}
-          name={name}
-          imageUrl={image}
-          className="size-[140px] rounded-full object-cover"
-          fallbackClassname="text-6xl fond-bolder"
-        />
+    <>
+      <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
+        <div className="flex flex-col items-start gap-4 lg:flex-row">
+          <UserAvatar
+            id={_id}
+            name={name}
+            imageUrl={image}
+            className="size-[140px] rounded-full object-cover"
+            fallbackClassname="text-6xl fond-bolder"
+          />
 
-        <div className="mt-3">
-          <h2 className="h2-bold ">{name}</h2>
-          <p className="paragraph-regular text-[#0f1117] dark:text-[#f4f6f8] ">
-            @{username}
-          </p>
-
-          <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
-            {portfolio && (
-              <ProfileLink
-                imgUrl="/icons/link.svg"
-                href={portfolio}
-                title="Portfolio"
-              />
-            )}
-            {location && (
-              <ProfileLink imgUrl="/icons/location.svg" title="Portfolio" />
-            )}
-            <ProfileLink
-              imgUrl="/icons/calendar.svg"
-              title={dayjs(createdAt).format("MMMM YYYY")}
-            />
-          </div>
-
-          {bio && (
-            <p className="paragraph-regular  text-[#212734] dark:text-[#f4f6f8] mt-8">
-              {bio}
+          <div className="mt-3">
+            <h2 className="h2-bold ">{name}</h2>
+            <p className="paragraph-regular text-[#0f1117] dark:text-[#f4f6f8] ">
+              @{username}
             </p>
+
+            <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
+              {portfolio && (
+                <ProfileLink
+                  imgUrl="/icons/link.svg"
+                  href={portfolio}
+                  title="Portfolio"
+                />
+              )}
+              {location && (
+                <ProfileLink imgUrl="/icons/location.svg" title="Portfolio" />
+              )}
+              <ProfileLink
+                imgUrl="/icons/calendar.svg"
+                title={dayjs(createdAt).format("MMMM YYYY")}
+              />
+            </div>
+
+            {bio && (
+              <p className="paragraph-regular  text-[#212734] dark:text-[#f4f6f8] mt-8">
+                {bio}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+          {loggedInUser?.user?.id === id && (
+            <Link href="/profile/edit">
+              <Button className="paragraph-medium bg-[#f4f6f8] dark:bg-[#212734] text-[#151821] dark:text-white min-h-12 min-w-44 px-4 py-3">
+                Edit Profile
+              </Button>
+            </Link>
           )}
         </div>
-      </div>
-
-      <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
-        {loggedInUser?.user?.id === id && (
-          <Link href="/profile/edit">
-            <Button className="paragraph-medium bg-[#f4f6f8] dark:bg-[#212734] text-[#151821] dark:text-white min-h-12 min-w-44 px-4 py-3">
-              Edit Profile
-            </Button>
-          </Link>
-        )}
-      </div>
-    </section>
+      </section>
+      <Stats
+        totalQuestions={totalQuestions}
+        totalAnswers={totalAnswers}
+        badges={{ GOLD: 0, BRONZE: 0, SILVER: 0 }}
+      />
+    </>
   );
 };
 
---
+++ components/user/Stats.tsx
@@ -0,0 +1,72 @@
+import { formatNumber } from "@/lib/utils";
+import { BadgeCounts } from "@/types/global";
+import Image from "next/image";
+import React from "react";
+
+interface Props {
+  totalQuestions: number;
+  totalAnswers: number;
+  badges: BadgeCounts;
+}
+
+interface StatsCardProps {
+  imgUrl: string;
+  value: number;
+  title: string;
+}
+
+const StatsCard = ({ imgUrl, value, title }: StatsCardProps) => (
+  <div className="border-[#f4f6f8] dark:border-[#151821] bg-white dark:bg-[#151821] flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 shadow-[-10px_10px_20px_0px_rgba(218_213_213_0.1)]  dark:shadow-[2px_0px_20px_0px_rgba(39_36_36_0.04)]">
+    <Image src={imgUrl} alt={title} width={40} height={50} />
+    <div>
+      <p className="paragraph-semibold text-[#0f1117] dark:text-white">
+        {value}
+      </p>
+      <p className="body-medium text-[#151821] dark:text-[#dce3f1]">{title}</p>
+    </div>
+  </div>
+);
+const Stats = ({ totalQuestions, totalAnswers, badges }: Props) => {
+  return (
+    <div className="mt-3 ">
+      <h4 className="h3-semibold text-[#0f1117] dark:text-white ">Stats</h4>
+      <div className="mt-5 grid grid-cols-2 gap-5 xs:grid-cols-2 md:grid-cols-4">
+        <div className="border-[#f4f6f8] dark:border-[#151821] bg-white dark:bg-[#151821] flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 shadow-[-10px_10px_20px_0px_rgba(218_213_213_0.1)]  dark:shadow-[2px_0px_20px_0px_rgba(39_36_36_0.04)]">
+          <div>
+            <p className="paragraph-semibold text-[#0f1117] dark:text-white">
+              {formatNumber(totalQuestions)}
+            </p>
+            <p className="body-medium text-[#212734] dark:text-[#dce3f1]">
+              Pertanyaan
+            </p>
+          </div>
+          <div>
+            <p className="paragraph-semibold text-[#0f1117] dark:text-white">
+              {formatNumber(totalAnswers)}
+            </p>
+            <p className="body-medium text-[#212734] dark:text-[#dce3f1]">
+              Jawaban
+            </p>
+          </div>
+        </div>
+        <StatsCard
+          imgUrl="/icons/gold-medal.svg"
+          value={badges.GOLD}
+          title="Gold Badges"
+        />
+        <StatsCard
+          imgUrl="/icons/silver-medal.svg"
+          value={badges.SILVER}
+          title="Silver Badges"
+        />
+        <StatsCard
+          imgUrl="/icons/bronze-medal.svg"
+          value={badges.BRONZE}
+          title="Bronze Badges"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Stats;
